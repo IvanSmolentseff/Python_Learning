4ff358a3adb327c2efb442dc05ddac87c1dff247
IvanSmolentseff <112988035+IvanSmolentseff@users.noreply.github.com>
2022-11-27 01:21:14 +0300

    Webinar_1 HomeTask_1

---
+++ Webinar_1_Home_Task/task_1.py
@@ -0,0 +1,10 @@
+'''Поработайте с переменными, создайте несколько, выведите на экран,
+запросите у пользователя несколько чисел и строк и сохраните в переменные,
+выведите на экран.'''
+
+a = input("Enter your name: ")
+b = int(input("Enter your age: "))
+c = float(input("Enter any number: "))
+
+print(a, b, c)
+print(f"Name:{a} - Age:{b} - Number:{c}")
