2991827e244a6caefa5ca2f7182d5e0637a852fb
IvanSmolentseff <112988035+IvanSmolentseff@users.noreply.github.com>
2022-11-27 22:56:44 +0300

    Webinar_2 HomeTask_1

--- Webinar_2_Home_Task/task_1.py
+++ Webinar_2_Home_Task/task_1.py
@@ -10,11 +10,10 @@ my_bool = False
 my_none = None
 my_list = [' ', 5]
 my_tuple = ('text', 5)
-my_set = {1, 1, 1, 2, 4, 9, 4} # дубли сделал специально, но pylint ругается
+my_set = {1, 1, 1, 2, 4, 9, 4}  # дубли сделал специально, но pylint ругается
 my_dict = {"name": 'Ivan', 'age': 33}
 
 result_list = [my_int, my_float, my_str, my_bool,
                my_none, my_list, my_tuple, my_set, my_dict]
 for i in result_list:
     print(f'{i} is {type(i)}')
-    
