d54d0343834da7cdffacd843e5aa632fcc9300c0
IvanSmolentseff <112988035+IvanSmolentseff@users.noreply.github.com>
2022-11-28 03:17:15 +0300

    Webinar_2 HomeTask_4

--- Webinar_2_Home_Task/task_4.py
+++ Webinar_2_Home_Task/task_4.py
@@ -5,7 +5,7 @@
 my_list = input('Enter few words separated by a space: ').split()
 
 for index, word in enumerate(my_list, 1):
-    if len(word)<=10:
+    if len(word) <= 10:
         print(f'{index}) {word}')
     else:
         print(f'{index}) {word[0:10]}')
