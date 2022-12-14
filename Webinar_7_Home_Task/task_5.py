213e68cc5066f54b04dff2e231e2b6c55f17084d
IvanSmolentseff <112988035+IvanSmolentseff@users.noreply.github.com>
2022-12-14 23:44:45 +0300

    Webinar_7 HomeTask_54

---
+++ Webinar_7_Home_Task/task_5.py
@@ -0,0 +1,50 @@
+'''Реализовать класс Stationery (канцелярская принадлежность). Определить в
+нем атрибут title (название) и метод draw (отрисовка). Метод выводит сообщение
+“Запуск отрисовки.” Создать три дочерних класса Pen (ручка), Pencil (карандаш),
+Handle (маркер). В каждом из классов реализовать переопределение метода draw.
+Для каждого из классов метод должен выводить уникальное сообщение. Создать
+экземпляры классов и проверить, что выведет описанный метод для каждого
+экземпляра.'''
+
+
+class Stationary:
+    '''Base class'''
+
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        '''
+        method of draw
+        :return: result
+        '''
+        return 'Start rendering'
+
+
+class Pen(Stationary):
+    '''Derived class'''
+
+    def draw(self):
+        return f'Rendering by pen "{self.title}" is not bad'
+
+
+class Pencil(Stationary):
+    '''Derived class'''
+
+    def draw(self):
+        return f'Rendering by pencil "{self.title}" is impossible'
+
+
+class Handle(Stationary):
+    '''Derived class'''
+
+    def draw(self):
+        return f'Rendering by handle "{self.title}" is best'
+
+
+pen = Pen('Berlingo')
+pencil = Pencil('Erich Krause')
+handle = Handle('Sketch&Art')
+print(pen.draw())
+print(pencil.draw())
+print(handle.draw())
